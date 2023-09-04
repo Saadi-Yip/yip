4d325272a02d033c3faf6ead06c26b713fcde5a2
Saadi-Yip <saadshabbir@leilanitech.com>
2023-09-04 13:05:39 +0500

    pagination issue needed to be crawl

--- components/blog/all-blogs/Blog.tsx
+++ components/blog/all-blogs/Blog.tsx
@@ -29,30 +29,34 @@ const Blog = ({
   const updatedExcerpt =
     excerpt && excerpt.length > 145 ? excerpt?.slice(0, 145) + "..." : excerpt;
   return (
-    <div className={styles.blog}>
-      <div className={styles.image}>
-        <Img
-          src={image}
-          alt="Internet service provider"
-          sizes={{
-            default: [14, 13],
-            mobile: [27, 33],
-          }}
-        />
-      </div>
-      <div className={styles.content}>
-        <h3>{updatedHeading}</h3>
-        <p>{updatedExcerpt}</p>
-        <div className={styles.bottom}>
-          <span className={styles.date_time}>
-            {dateConverter(createdAt)}· {readTime}
-          </span>
-          <div className={styles.blog_categories}>
-            <span>{category?.name}</span>
+    
+      <Link href={`blog/${slug}`}>
+        <div className={styles.blog}>
+          <div className={styles.image}>
+            <Img
+              src={image}
+              alt="Internet service provider"
+              sizes={{
+                default: [14, 13],
+                mobile: [27, 33],
+              }}
+            />
+          </div>
+          <div className={styles.content}>
+            <h3>{updatedHeading}</h3>
+            <p>{updatedExcerpt}</p>
+            <div className={styles.bottom}>
+              <span className={styles.date_time}>
+                {dateConverter(createdAt)}· {readTime}
+              </span>
+              <div className={styles.blog_categories}>
+                <span>{category?.name}</span>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </Link>
+   
   );
 };
 
